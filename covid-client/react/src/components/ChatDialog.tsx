1baec0b26ecd339a1bc6ed5c0721b9cdde61a16a
Nattapong Sirilappanich <nattapong.sirilappanich2@ibm.com>
2020-04-25 16:12:20 +0700

    Fix #6

--- covid-client/react/src/components/ChatDialog.tsx
+++ covid-client/react/src/components/ChatDialog.tsx
@@ -6,6 +6,12 @@ import { Container } from '@material-ui/core'
 import ChatMessage from './ChatMessage'
 import { Message, MessageState } from './commons/Message'
 
+// Typical url pattern which may be part of <a href="url"></a> tag.
+// It need to match only up to ending of quote.
+// If there's space in url, then it's consider invalid url.
+// If it's not contained within quote, it'll consider valid url upto first space.
+const url_pattern = /(?<=(['"]?))(https?:\/\/[^\s]*)(?=\1)/gi;
+
 export default function ChatDialog({messages, style, onChoose}: {style?: React.CSSProperties, messages: Message[], onChoose?: (msg: string) => void}) {
 
     // reference point for scrolling. It currently on bottom of this component
@@ -56,25 +62,41 @@ export default function ChatDialog({messages, style, onChoose}: {style?: React.C
                         </React.Fragment>
                     )
                 } else {
-                    if (m.message.startsWith("<a ")) {
-                        // link to be preview message
-                        let urlIdx = m.message.toLowerCase().indexOf("href=\"") + 6
-                        let urlEndIdx = m.message.indexOf("\"", urlIdx + 1)
-                        
-                        // not empty href attribute
-                        if (urlIdx < urlEndIdx) {
-                            let url = new URL(m.message.substring(urlIdx, urlEndIdx))
-                            m.previewUrl = url
-                        }
+                    let urls = m.message.match(url_pattern)
+                    // implement #6 by replace below code chunk with new one below
 
-                        return <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
-                    } else if (m.message.startsWith("http")) {
-                        let url = new URL(m.message)
-                        m.message = url.hostname
-                        m.previewUrl = url
-                        return <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
+                    // if (m.message.startsWith("<a ")) {
+                    //     // link to be preview message
+                    //     let urlIdx = m.message.toLowerCase().indexOf("href=\"") + 6
+                    //     let urlEndIdx = m.message.indexOf("\"", urlIdx + 1)
+                        
+                    //     // not empty href attribute
+                    //     if (urlIdx < urlEndIdx) {
+                    //         let url = new URL(m.message.substring(urlIdx, urlEndIdx))
+                    //         m.previewUrl = url
+                    //     }
+
+                    //     return <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
+                    // } else if (m.message.startsWith("http")) {
+                    //     let url = new URL(m.message)
+                    //     m.message = url.hostname
+                    //     m.previewUrl = url
+                    //     return <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
+                    // } else {
+                    //     // simple reply, may have some option for user to pick 
+                    //     return <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
+                    // }
+                    if (urls && urls.length > 0) {
+                        console.debug("Found following urls to be preview", urls)
+                        return (
+                        <>
+                            <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
+                            {urls.map((u, i) => (
+                                <ChatMessage key={m.uuid} message={{previewUrl: new URL(u), uuid: m.uuid + "-" + i, timestamp:new Date(), state:MessageState.succeed, message: "", type: "Watson"}} onChoose={onChoose}/>
+                            ))}
+                        </>
+                        )
                     } else {
-                        // simple reply, may have some option for user to pick 
                         return <ChatMessage key={m.uuid} message={m} onChoose={onChoose}/>
                     }
                 }
